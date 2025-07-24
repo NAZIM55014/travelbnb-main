c166fafc4940e704f15a8875dccb5215bd7df19f
NAZIM55014 <mdnazimmth584@gmail.com>
2025-07-24 08:52:23 +0530

    Initial commit

---
+++ src/main/java/com/travelbnb/payload/LoginDto.java
@@ -0,0 +1,27 @@
+package com.travelbnb.payload;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class LoginDto {
+    private String username;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
